d1201747ce3a39f65e12dd4a0eb8c077f742fce7
trompot <trompot.mydns.jp@gmail.com>
2018-06-23 15:44:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
-(function (handleload) {
-            var xhr = new XMLHttpRequest;
-          
-            xhr.addEventListener('load', handleload, false);
-            xhr.open('GET', 'json/infomation.json', true);
-            xhr.send(null);
-          }(function handleLoad (event) {
-            var xhr = event.target,
-                obj = JSON.parse(xhr.responseText);
-                console.log(obj.shop);
-                initVue(obj);
-          }));
-          function initVue(info){
+(function(handleload) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", handleload, false);
+    xhr.open("GET", "json/infomation.json", true);
+    xhr.send(null);
+})(function handleLoad(event) {
+    var xhr = event.target,
+    obj = JSON.parse(xhr.responseText);
+    initVue(obj);
+});
+
+function initVue(info) {
     new Vue({
-        el: '#pc',
+        el: "#main",
         data: {
             infos: info["rabbit"]
+        }
+    });
+    new Vue({
+        el: "#server",
+        data: {
+            infos: info["server"]
         }
     });
 }
